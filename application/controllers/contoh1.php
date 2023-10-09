929a75c0e44cc948a753d095fc40091239245734
Tigornarotama <Tigornarotama@gmail.com>
2023-10-09 16:31:37 +0700

    menambahkan web dengan codeigniture

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,8 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"nama saya Tigor Narotama
+        saya tinggal didaerah wates kulon progo";
